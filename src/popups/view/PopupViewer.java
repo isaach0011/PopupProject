19cb08b18a92f0326f861b8791ec99c5846ac437
isaach0011 <ihil17523@740s-pl-177.local>
2016-09-29 12:54:01 -0600

    Added the viewer so now popups can come up.

--- src/popups/view/PopupViewer.java
+++ src/popups/view/PopupViewer.java
@@ -1,6 +1,45 @@
 package popups.view;
 
+import javax.swing.JOptionPane;
+
+/**
+ * Popup display class with three methods for GUI.
+ * @author ihil17523
+ * @version September 27, 2016
+ */
+
 public class PopupViewer
 {
+	/**
+	 * Creates a popup to show the user a String.
+	 * @param message The String to show the user.
+	 */
+	public void displayMessage(String message)
+	{
+		JOptionPane.showMessageDialog(null, message);
+	}
 	
+	/**
+	 * Creates a popup to ask a question to the user. Stores the response
+	 * as a String.
+	 * @param question The question supplied to the user.
+	 * @return The users response as a String.
+	 */
+	public String collectResponse(String question)
+	{
+		String response = " ";
+		
+		response = JOptionPane.showInputDialog(null, question);
+		
+		return response;
+	}
+	
+	public int collectOption(String question)
+	{
+		int optionNumber = 0;
+		
+		optionNumber = JOptionPane.showConfirmDialog(null, question);
+		
+		return optionNumber;
+	}
 }
